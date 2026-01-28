ddcf9cd7d4f4f171f9258026c9a8efbfa3846176
Victor Shirinskiy <VictorVance@yandex.ru>
2026-01-29 00:12:23 +0700

    Add login tests with Page Object Model

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import base.BaseTest;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+
+public class LoginTests extends BaseTest {
+
+    @Test
+    void successfulLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+        assert loginPage.isLoginSuccessful();
+    }
+    @Test
+    void loginWithWrongPassword() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "wrong_password");
+
+        assert loginPage.isErrorDisplayed();
+    }
+    @Test
+    void loginWithLockedUser() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("locked_out_user", "secret_sauce");
+
+        assert loginPage.isErrorDisplayed();
+    }
+    @Test
+    void loginWithEmptyFields() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("", "");
+
+        assert loginPage.isErrorDisplayed();
+    }
+    @Test
+    void loginWithPerformanceGlitchUser() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("performance_glitch_user", "secret_sauce");
+
+        assert loginPage.waitForSuccessfulLogin();
+    }
+
+
+}
